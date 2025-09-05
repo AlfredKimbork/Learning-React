11a9295a1e71782a55ff965e62b8f7bed127ae93
Alfred Kimbork <kimborkalfred@gmail.com>
2025-09-05 10:06:55 +0200

    props and outputting a list

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import {useState} from "react"
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [name, setName] = useState("Alfred")
     const handleClick = () => name == "Alfred" ? setName("David") : setName("Alfred");
+    const [blogs, setBlogs] = useState([
+        {title: "My first webpage!", body: "Lorem ipsum...", author: "William", id: 1}, 
+        {title: "Why crabs are the best animal", body: "Lorem ipsum...", author: "Bertil", id: 2},   
+        {title: "The scam we all fall for", body: "Lorem ipsum...", author: "Marco", id: 3} 
+    ])
 
     return (
         <div className="home">
             <h2>Welcome, {name}</h2>
-            <button onClick={handleClick}>click me to change name</button>
+            <BlogList blogs={blogs} title="All Blogs"/>
+            <button onClick = {handleClick}>Click me!</button>
         </div>
     );
 }

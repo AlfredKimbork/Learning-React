4093d61fc086ca3ec5445e93ebb53aab9f0a48cd
Alfred Kimbork <kimborkalfred@gmail.com>
2025-09-05 08:25:26 +0200

    removed template and made teh initial page

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import './css/navbar.css'
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Blog</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a className="create" href="/create">New Blog</a></li>
+            </ul>
+        </nav>
+    );
+}
+ 
+export default Navbar;
